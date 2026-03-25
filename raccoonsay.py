96ddd6d726839fb9fc7033523d573baab539e148
rsdraghi <rsdraghi@gmail.com>
2026-03-25 05:01:06 +0000

    EC midterm

---
+++ raccoonsay.py
@@ -0,0 +1,13 @@
+## midterm EC raccoonsay ##
+
+phrase = str(input("Hello! Please enter a phrase: "))
+
+print("<", phrase, " >")
+
+print(r"-----------------")
+print(r"\  /\_/\  ")
+print(r" \( o.o )_______")
+print(r"     -          }")
+print(r"     \________\ \ ")
+print(r"      ||   ||  \/") 
+                       
